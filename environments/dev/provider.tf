676e2d507396d5c0018975aaffcfc146450e4aae
Amit Kumar <amit.chaudhary0687@gmail.com>
2025-07-13 22:55:11 +0530

    updated provider.tf file

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,7 +1,7 @@
 terraform {
-  required_prviders {
+  required_providers {
     azurerm = {
-      source  = "hashicorp.com/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.20.0"
     }
   }
